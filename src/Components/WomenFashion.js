6455dcfcbf80a242df7a07e56155c744337f2259
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-07-21 17:50:22 +0500

    Added Images and links to the Brands(daraz, elo)

--- src/Components/WomenFashion.js
+++ src/Components/WomenFashion.js
@@ -2,8 +2,126 @@ import React from 'react'
 
 export default function WomenFashion() {
     return (
-        <div>
+        <div className="container-wrapper">
+        <div className="portfolio-items-wrapper">
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/FrontDetail_2000x.jpg)", 
+                    alt: "Women's Dress"}}>
+                </div>
+                
+                <div className="img-text-wrapper">  
+                    <div className="logo-wrapper">
+                        <img src="https://image3.mouthshut.com/images/imagesp/925917207s.jpg" alt="Women's Dress" />
+                    </div>
+                    <div className="subtitle">
+                       <h3>  
+                            <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.daraz.pk/womens-dresses/?spm=a2a0e.home.cate_8_8.1.35e34937ny6ik9"> Women's Dress </a>
+                           </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://static-01.daraz.pk/p/mdc/68967cb658b58f4c517c9dd4f433134d.jpg)", 
+                    alt: "Women's T-shirts"}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://image3.mouthshut.com/images/imagesp/925917207s.jpg" alt="Women's T-shirts" />
+                    </div>
+                    <div className="subtitle">
+                       <h3>  
+                            <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.daraz.pk/womens-t-shirts/?spm=a2a0e.home.cate_8_4.3.35e34937ny6ik9"> Women's T-shirts </a>
+                           </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://static-01.daraz.pk/original/75625750fafc00341ea67cb5753c939b.jpg)",
+                    alt:" Women's Sweater"}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://image3.mouthshut.com/images/imagesp/925917207s.jpg" alt=" Women's Sweater" />
+                    </div>
+                    <div className="subtitle">
+                       <h3>  
+                            <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.daraz.pk/womens-sweaters/?spm=a2a0e.home.cate_8.9.35e34937ny6ik9"> Women's Sweater </a>
+                           </h3>
+                    </div>
+                </div>
+            </div>
+
             
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/sdf_f804b7fb-55ed-41e9-bc6f-e900b682862f_2000x.jpg?v=1593218138)",
+                    alt:"Women's T-Shirts "}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://i.pinimg.com/originals/61/e1/e9/61e1e92d1cba837bba6f64f1a03a9b8e.png" alt="Women's T-Shirts " />
+                    </div>
+                    <div className="subtitle">
+                       <h3>  
+                            <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.exportleftovers.com/collections/women-tees-polos"> Women's T-Shirts </a>
+                           </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/Front-2_2000x.jpg?v=1589178159)", 
+                    alt:"Kurtis & Lawn "}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://i.pinimg.com/originals/61/e1/e9/61e1e92d1cba837bba6f64f1a03a9b8e.png" alt="Kurtis & Lawn " />
+                    </div>
+                    <div className="subtitle">
+                       <h3>  
+                            <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.exportleftovers.com/collections/women-kurtis-lawn"> Kurtis & Lawn </a>
+                           </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/graphite_79710323-3f6a-4087-afce-948a547a317a_2000x.jpg?v=1578578523)",
+                    alt:"Jackets"}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://i.pinimg.com/originals/61/e1/e9/61e1e92d1cba837bba6f64f1a03a9b8e.png" alt="Jackets" />
+                    </div>
+                    <div className="subtitle">
+                       <h3>  
+                            <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.exportleftovers.com/collections/women-sweat-shirts-jackets"> Jackets </a>
+                           </h3>
+                    </div>
+                </div>
+            </div>
+
         </div>
+    </div>    
     )
 }
