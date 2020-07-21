f5a7c2ae2b572acb368bc0bf52e41a6764dcb824
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-07-21 17:50:40 +0500

    Update Mynav.js

--- src/NavBar/Mynav.js
+++ src/NavBar/Mynav.js
@@ -4,16 +4,13 @@ import '../styles.css';
 
 export default function Mynav() {
     return (
-        <div>
             <nav className="nav">
                 <ul className="nav-links">
                     <Link className="link" to='/'>           Home           </Link>
                     <Link className="link" to='/brand'>      Brands         </Link>
                     <Link className="link" to='/category'>   Categories     </Link> 
-                    <Link className="link" to='/about' >     Site Info      </Link>
+                    <Link className="link" to='/about' >     About      </Link>
                 </ul>
-               
             </nav>
-        </div>
     )
 }
