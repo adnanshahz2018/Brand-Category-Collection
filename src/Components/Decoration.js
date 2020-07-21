92ba4001c6133e50d88beda644bddd46f49e8f9b
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-07-21 17:50:08 +0500

    Added Images and links to the Brands(daraz, elo)

---
+++ src/Components/Decoration.js
@@ -0,0 +1,126 @@
+import React from 'react'
+
+export default function Decoration() {
+    return (
+        <div className="container-wrapper">
+        <div className="portfolio-items-wrapper">
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://static-01.daraz.pk/original/dc52515655b44415db4663c6a7ea24a7.jpg)",
+                    alt:"Bedroom"}}>
+                </div>
+                
+                <div className="img-text-wrapper">  
+                    <div className="logo-wrapper">
+                        <img src="https://image3.mouthshut.com/images/imagesp/925917207s.jpg" alt="Bedroom" />
+                    </div>
+                    <div className="subtitle">
+                        <h3> 
+                           <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.daraz.pk/bedding-2/?spm=a2a0e.home.cate_7.2.35e34937jvMXKt">Bedroom</a> 
+                        </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://static-01.daraz.pk/p/fb2670cf313c71893c46a3fb91e804e3.jpg)",
+                    alt:"Curtains"}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://image3.mouthshut.com/images/imagesp/925917207s.jpg" alt="Curtains" />
+                    </div>
+                    <div className="subtitle">
+                        <h3> 
+                           <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.daraz.pk/curtains-blinds-shades/?spm=a2a0e.home.cate_7_3.4.35e34937jvMXKt">Curtains</a> 
+                        </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://static-01.daraz.pk/p/e180910b171d01853560a42caad7e0f9.jpg)",
+                    alt:"Drawing & Dining "}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://image3.mouthshut.com/images/imagesp/925917207s.jpg" alt="Drawing & Dining" />
+                    </div>
+                    <div className="subtitle">
+                        <h3> 
+                            <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.daraz.pk/catalog/?q=+dining&_keyori=ss&from=input&spm=a2a0e.searchlist.search.go.26971decWj3zJx"> Drawing & Dining </a>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/23067_1600x.jpg?v=1591184992)",
+                    alt:"Bed Sets"}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://i.pinimg.com/originals/61/e1/e9/61e1e92d1cba837bba6f64f1a03a9b8e.png" alt="Bed Sets" />
+                    </div>
+                    <div className="subtitle">
+                        <h3> 
+                           <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.exportleftovers.com/collections/bed-sets" >Bed Sets</a>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://static-01.daraz.pk/p/df0d8cf4bdb204dfd7b06ade75acbd92.jpg)",
+                    alt:"Mirrors"}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://i.pinimg.com/originals/61/e1/e9/61e1e92d1cba837bba6f64f1a03a9b8e.png" alt="Mirrors"/>
+                    </div>
+                    <div className="subtitle">
+                        <h3> 
+                           <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.daraz.pk/mirrors/?spm=a2a0e.home.cate_7_3.6.35e34937irHyf7" > Mirrors </a>
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            
+            <div className="portfolio-item-wrapper">
+                <div className="portfolio-img-background" 
+                    style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/02_41beeeeb-4c5a-4250-b63a-fd4397b42c13_2000x.jpg)",
+                    alt:"Hanging Shoe Organizer"}}>
+                </div>
+                
+                <div className="img-text-wrapper">
+                    <div className="logo-wrapper">
+                        <img src="https://i.pinimg.com/originals/61/e1/e9/61e1e92d1cba837bba6f64f1a03a9b8e.png" alt="Hanging Shoe Organizer" />
+                    </div>
+                    <div className="subtitle">
+                       <h3> 
+                           <a target="_blank" rel="noopener noreferrer"
+                           href="https://www.exportleftovers.com/collections/decorations" > Hanging Shoe Organizer </a>
+                       </h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>    
+
+    )
+}
