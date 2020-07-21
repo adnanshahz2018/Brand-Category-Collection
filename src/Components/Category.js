968d64675154540cecc9a7ff1e0025c70c4d5749
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-07-21 17:49:00 +0500

    Added sub categories of Men, Women and Decoration

--- src/Components/Category.js
+++ src/Components/Category.js
@@ -1,29 +1,24 @@
 import React, { Component } from 'react'
-import CategoryMainGrid from './CategoryMainGrid'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import MenFashion from './MenFashion'
 import WomenFashion from './WomenFashion'
 import CategorySidePanel from './CategorySidePanel'
+import Decoration from './Decoration'
 
 export class Category extends Component {
-
-    
-
     render() {
         return (
             <Router >
-            <div>
-            <div className="left tab">
+            <div className="left">
                 <CategorySidePanel />
             </div>
            
             <div className="right">
-                <Route exact path="/category/menfashion" Component={MenFashion} >        <MenFashion />      </Route>
+                <Route exact path="/category" Component={MenFashion} >        <MenFashion />      </Route>
                 <Route exact path="/category/womenfashion" Component={WomenFashion} >    <WomenFashion />    </Route>
-                <Route exact path="/category" Component={CategoryMainGrid} > <CategoryMainGrid />    </Route>
+                <Route exact path="/category/decoration" Component={Decoration} > <Decoration />    </Route>
              </div>
  
-         </div>
          </Router>
         )
     }
