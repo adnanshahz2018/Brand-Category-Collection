85b1c5fbf15b866920a079cb364e1374c588aecb
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-07-28 16:15:04 +0500

    Moved file to the directory { Category }

---
+++ src/Components/Category/CategorySidePanel.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import '../../styles.css'
+
+export default function CategorySidePanel() {
+    return (
+            <table className="productlinks">
+                <tr className="panel_links" >
+                    <Link className="link" to='/category'>   Men     </Link> 
+                </tr>
+                <tr className="panel_links" >
+                   <Link className="link"  to='/category/womenfashion'>   Women    </Link> 
+                </tr>
+                <tr className="panel_links" >
+                   <Link className="link" to='/category/decoration' >     Decor   </Link>
+                </tr>
+               </table>
+    )
+}
