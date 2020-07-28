7b78504d98057ef1ac291b003ccdb115ba590190
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-07-28 16:12:23 +0500

    Brand of ExportLeftOver

---
+++ src/Components/Brand/Elo.js
@@ -0,0 +1,102 @@
+import React from 'react'
+import './brandgridstyles.css'
+
+export default function Elo() {
+    return (
+        <div className="container-wrapper">
+            <div className="portfolio-items-wrapper">
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/6_f9112491-656a-413e-a773-bcbf3e9c89ef_2000x.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.exportleftovers.com/collections/men"> Men's Fashion</a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/FrontDetail_2000x.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.exportleftovers.com/collections/women"> Women's Fashion </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+                
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/02_41beeeeb-4c5a-4250-b63a-fd4397b42c13_2000x.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.exportleftovers.com/collections/decorations"> Decoration </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>  
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/asdfxcv_d5ba4a8a-59b4-41d1-afc7-b35f7aeee6ad_2000x.jpg?v=1595397915)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://exportleftovers.com/collections/new-arrivals"> New Arrivals </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>  
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/2_f691f8c9-1d71-4746-8f64-795b1d4398dd_2000x.png?v=1576651701)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.exportleftovers.com/collections/special-deals"> Special Deals </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>  
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://cdn.shopify.com/s/files/1/0671/0621/products/00001_64_2000x.jpg?v=1568821548)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.exportleftovers.com/collections/b-quality"> Minor Fault </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>  
+
+
+            </div>
+        </div>    
+    )
+}
