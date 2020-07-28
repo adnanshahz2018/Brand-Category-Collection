ee2c255cd8c1c28e9ffd01d16ef1434f1f173506
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-07-28 16:12:07 +0500

    Brand of Daraz

---
+++ src/Components/Brand/Daraz.js
@@ -0,0 +1,105 @@
+import React from 'react'
+import './brandstyles.css'
+
+export default function Daraz() {
+    return (
+        <div className="container-wrapper">
+            <div className="portfolio-items-wrapper">
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://static-01.daraz.pk/p/33f7c1373ef499d6310a76e8adcc98bd.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.daraz.pk/mens-t-shirts/?spm=a2a0e.home.cate_9.1.35e34937wLPLhb"> Men's Fashion </a>
+                            </h3>
+                        </div>
+                    </div>
+
+                </div>
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://static-01.daraz.pk/p/mdc/68967cb658b58f4c517c9dd4f433134d.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.daraz.pk/womens-traditional-clothing/?spm=a2a0e.home.cate_8.1.35e34937wLPLhb"> Women's Fashion </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://static-01.daraz.pk/p/e180910b171d01853560a42caad7e0f9.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.daraz.pk/home-decoration/?spm=a2a0e.home.cate_7.3.35e34937wLPLhb"> Decoration </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://static-01.daraz.pk/p/0bd84ea1c9b0c078c145c4be396a3bef.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.daraz.pk/health-beauty-tools/?spm=a2a0e.home.cate_4.2.5c9b4937Rff1PM"> Beauty Tools </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://static-01.daraz.pk/p/2964b3d16922b66edd92a5790940cc7f.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.daraz.pk/consumer-electronics/?spm=a2a0e.home.cate_3.1.5c9b4937Rff1PM"> Consumer Electronics </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="portfolio-item-wrapper">
+                    <div className="portfolio-img-background" 
+                        style={{backgroundImage: "url(https://static-01.daraz.pk/p/68b503dd456b0558be395242bbb6f6f7.jpg)"}}>
+                    </div>
+                    
+                    <div className="img-text-wrapper">
+                        <div className="subtitle">
+                            <h3>  
+                                <a target="_blank" rel="noopener noreferrer"
+                                href="https://www.daraz.pk/furniture/?spm=a2a0e.home.cate_7.4.5c9b4937Rff1PM"> Home & Life Style </a>
+                            </h3>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>    
+    )
+}
+
+    
