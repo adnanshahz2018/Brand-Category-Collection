493bf2a3901f89da01bf8c3f8d29fbb468625893
Adnan Shah <43321679+adnanshahz2018@users.noreply.github.com>
2020-09-20 10:47:35 +0500

    Update

--- src/Components/About.js
+++ src/Components/About.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function About() {
     return (
         <div>
-            Here we will provide the Website information contact details.
+            Here we will provide the Website information and contact details.
         </div>
     )
 }
